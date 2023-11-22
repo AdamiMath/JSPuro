aa5da7e0935cc2e6d3991dc65c56dfdafee18e08
matheusadami.lima@gmail.com <matheusadami.lima@gmail.com>
2023-11-22 16:18:35 -0300

    update

--- script.js
+++ script.js
@@ -3,94 +3,104 @@ let endereco = document.getElementById('endereco');
 let select = document.getElementById('saboresPizza');
 let checkbox = document.getElementById('querRefrigerante');
 let button = document.getElementById('button');
-
-
+let cardPedidos = document.getElementById('card-pedidos');
 
 let objetos = [];
-let contador = 1;
 
+// Recuperar objetos do localStorage, se existirem
+const objetosSalvos = JSON.parse(localStorage.getItem('objetos')) || [];
 
+// Adicionar os objetos salvos à lista de objetos
+objetos = objetosSalvos;
 
-function criarObjeto(e){
-    e.preventDefault();
-    let ValorCliente = nomeCliente.value;
-    let valorendereco = endereco.value;
-    let saborPizza = select.value;
-    let QuerRefrigerante = checkbox.checked;
-
-
-    if(QuerRefrigerante){
-        QuerRefrigerante = 'Quer Refigerante'
-    }else{
-        QuerRefrigerante = 'Não Quer Refrigerante'
-    }
-    let nomeObj = 'objeto' + contador;
-    let novoObj = new PedidoCliente(ValorCliente,valorendereco,saborPizza,QuerRefrigerante);
-    objetos.push({nome: nomeObj, objeto: novoObj});
-    contador++;
-
-    if(novoObj.nome.trim() === ''){
-        document.getElementById('mensagemErro').textContent = 'Por favor, preencha o campo Nome do Cliente.';
-        return undefined;
-    }else if(novoObj.endereco.trim() === ''){
-        document.getElementById('mensagemErro').textContent = 'Por favor, preencha o campo Com o Endereço.';
-        return undefined;
-    }else{
-        let novaDiv = document.createElement('div');
-        novaDiv.classList.add('formularioPedido');
-        // Iterar sobre as propriedades do objeto e criar parágrafos
-        for (let propriedade in novoObj) {
-            if (novoObj.hasOwnProperty(propriedade)) {
-            let novoParagrafo = document.createElement('p');
-            novoParagrafo.textContent = propriedade + ': ' + novoObj[propriedade];
-            novaDiv.appendChild(novoParagrafo);
-            }
-        }
-            // Adicionar a div ao corpo do documento
-            document.body.appendChild(novaDiv);
-            nomeCliente.value='';
-            endereco.value='';
-            document.getElementById('mensagemErro').textContent='';
-            novoObj.createButton(novaDiv); 
-            
-            
-   
-                 
-    }
+function salvarObjetosNoLocalStorage() {
+    // Salvar objetos no localStorage
+    localStorage.setItem('objetos', JSON.stringify(objetos));
 }
 
-
 class PedidoCliente {
-    constructor (nomeCliente, endereco,select,checkbox){
+    constructor(nomeCliente, endereco, select, checkbox) {
         this.nome = nomeCliente;
         this.endereco = endereco;
         this.pizza = select;
         this.refrigerante = checkbox;
+        this.div = null;
     }
 
-    createButton(div){
+    createDiv() {
+        this.div = document.createElement('div');
+        this.div.classList.add('formularioPedido');
+
+        for (let propriedade in this) {
+            if (this.hasOwnProperty(propriedade) && propriedade !== 'div' && propriedade !== 'createDiv') {
+                let novoParagrafo = document.createElement('p');
+                let valor = this[propriedade];
+                novoParagrafo.textContent = propriedade + ': ' + (typeof valor === 'object' ? JSON.stringify(valor) : valor);
+                this.div.appendChild(novoParagrafo);
+            }
+        }
+
+        cardPedidos.appendChild(this.div);
+
         const button = document.createElement('button');
         button.classList.add('btn-pronto');
-        button.textContent = "Está Pronto!"
-        div.appendChild(button);
+        button.textContent = "Está Pronto!";
+        this.div.appendChild(button);
+
         button.addEventListener('click', () => {
-            div.remove();
-            button.removeEventListener('click', () => {});
+            this.div.remove();
+            // Atualizar objetos após a remoção
+            objetos = objetos.filter(obj => obj.nome !== this.nome);
+            salvarObjetosNoLocalStorage();
         });
     }
-    
 }
 
+function criarDiv(objeto) {
+    objeto.createDiv();
+}
+
+function criarObjeto(e) {
+    e.preventDefault();
+
+    // Limpar mensagem de erro
+    document.getElementById('mensagemErro').textContent = '';
+
+    let valorCliente = nomeCliente.value.trim();
+    let valorEndereco = endereco.value.trim();
+    let saborPizza = select.value;
+    let querRefrigerante = checkbox.checked;
+
+    if (valorCliente === '') {
+        document.getElementById('mensagemErro').textContent = 'Por favor, preencha o campo Nome do Cliente.';
+        return undefined;
+    }
+
+    if (valorEndereco === '') {
+        document.getElementById('mensagemErro').textContent = 'Por favor, preencha o campo Com o Endereço.';
+        return undefined;
+    }
+
+    if (querRefrigerante) {
+        querRefrigerante = 'Quer Refrigerante';
+    } else {
+        querRefrigerante = 'Não Quer Refrigerante';
+    }
+
+    let novoObj = new PedidoCliente(valorCliente, valorEndereco, saborPizza, querRefrigerante);
+    objetos.push({ nome: 'objeto' + objetos.length, objeto: novoObj });
+
+    // Criar a div correspondente ao objeto e adicionar ao documento
+    criarDiv(novoObj);
+
+    // Salvar objetos no localStorage após cada criação
+    salvarObjetosNoLocalStorage();
+
+    // Limpar os campos de input
+    nomeCliente.value = '';
+    endereco.value = '';
+    // Limpar a seleção do checkbox, se necessário
+    checkbox.checked = false;
+}
 
 button.addEventListener('click', criarObjeto);
-
-
-
-
-
-
-
-
-
-
-
